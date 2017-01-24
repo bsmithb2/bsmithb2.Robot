af9b991d3239e441a364cfdf76c7e4c09bb8bcc1
Bradley Smith <bsmithb2@gmail.com>
2017-01-24 20:00:09 +1100

    Added dummy test for CI

--- bsmithb2.Robot.Tests/UnitTest1.cs
+++ bsmithb2.Robot.Tests/UnitTest1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using NUnit;
+using NUnit.Framework;
 
 namespace bsmithb2.Robot.Tests
 {
-   
+    [TestFixture]
     public class UnitTest1
     {
-        
+        [Test]
         public void TestMethod1()
         {
         }
