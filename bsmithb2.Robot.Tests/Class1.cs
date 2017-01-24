0402e3ff6b469f5be7a1166c4bcb80b688608b8c
Bradley Smith <bsmithb2@gmail.com>
2017-01-24 20:34:48 +1100

    And again

---
+++ bsmithb2.Robot.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bsmithb2.Robot.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
