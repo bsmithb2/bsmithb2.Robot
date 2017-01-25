56fadb976eeb39a2622b997366e27b5626880bd5
Bradley Smith <bsmithb2@gmail.com>
2017-01-25 19:45:47 +1100

    Application Test Framework

---
+++ bsmithb2.Robot.Tests/ApplicationTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bsmithb2.Robot.Tests
+{
+    [TestFixture]
+    public class ApplicationTests
+    {
+        [Test]
+        public void Constructor_ShouldAccept_ILogger()
+        {
+            
+        }
+    }
+}
